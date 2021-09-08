7eb59f297a19bfbd7fa25ae5d74170294f91b1f9
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-09 00:23:56 +0530

    Updated

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,14 +1,51 @@
 import { useState, useEffect } from 'react'
 import { useChatContext } from 'stream-chat-react'
 
+
+import { ResultsDropdown } from './'
 import { SearchIcon } from '../assets'
-const ChannelSearch = () => {
+
+
+const ChannelSearch = ({setToggleContainer}) => {
+    const { client, setActiveChannel } = useChatContext()
     const [query, setQuery] = useState('')
     const [loading, setLoading] = useState(false)
+    const [teamChannels, setTeamChannels] = useState([])
+    const [directChannels, setDirectChannels] = useState([])
+
+
+    useEffect(() => {
+        if(!query) {
+            setTeamChannels([]);
+            setDirectChannels([]);
+        }
+    }, [query])
+
 
     const getChannels = async (text) => {
         try {
-            //TODO Fetch Channels
+            const channelResponse = client.queryChannels({
+                type: 'team',
+                name: { $autocomplete: text },
+                members: { $in: [client.userID] }
+            });
+
+            const UserResponse = client.queryUsers({
+                id: { $ne: client.userID },
+                name: { $autocomplete: text }
+            });
+
+            const [channels, { users }] = await Promise.all([channelResponse, UserResponse])
+
+            if (channels.length) {
+                setTeamChannels(channels)
+            }
+
+            if (users.length) {
+                setDirectChannels(users)
+            }
+
+
         } catch (error) {
             setQuery('')
         }
@@ -20,6 +57,14 @@ const ChannelSearch = () => {
         setQuery(events.target.value)
         getChannels(events.target.value)
     }
+
+
+    const setChannel = (channel) => {
+        setQuery('');
+        setActiveChannel(channel);
+    }
+
+
     return (
         <div className="channel-search__container">
             <div className="channel-search__input__wrapper">
@@ -34,6 +79,16 @@ const ChannelSearch = () => {
                     onChange={onSearch}
                 />
             </div>
+            {query && (
+                <ResultsDropdown
+                    teamChannels={teamChannels}
+                    directChannels={directChannels}
+                    loading={loading}
+                    setChannel={setChannel}
+                    setQuery={setQuery}
+                    setToggleContainer={setToggleContainer}
+                />
+            )}
         </div>
     )
 }
--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twillioClient = require('twilio')(accountSid, authToken);
+
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +21,30 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if (type === 'message.new') {
+        members
+            .filter((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if (!user.online) {
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('Message sent!'))
+                        .catch((err) => console.log(err));
+                }
+            })
+
+        return res.status(200).send('Message sent!');
+    }
+
+    return res.status(200).send('Not a new message request');
+});
 
 app.use('/auth', authRoutes);
 
