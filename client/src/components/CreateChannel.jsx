7886e3519673bd1287043aa2c10d183a73082083
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-08 14:24:08 +0530

    Updated

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState }from 'react'
+import { useChatContext } from 'stream-chat-react' 
 
-const CreateChannel = () => {
+import {UserList} from'./'
+import { CloseCreateChannel } from '../assets'
+
+const ChannelNameInput = ({channelName ='',setChannelName})=>{
+    const handleChange = (e)=>{
+        e.preventDefault()
+        setChannelName(e.target.value)
+    }
     return (
-        <div>
-            Create a new channel
+        <div className="channel-name-input__wrapper">
+            <p>Name</p>
+            <input value={channelName} onChange={handleChange} placeholder="channel-name"/>
+            <p>Add Members</p>
+        </div>
+    )
+}
+
+const CreateChannel = ({createType, setIsCreating}) => {
+    const [channelName,setChannelName] = useState('')
+    return (
+        <div className="create-channel__container">
+            <div className="create-channel__header">
+                <p>{createType=== 'team'? 'Create a New Channel': 'Send a Direct Message' }</p>
+                <CloseCreateChannel setIsCreating={setIsCreating}/>
+            </div>
+            {createType=== 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
+            <UserList/>
         </div>
     )
 }
---
+++ client/src/components/UserList.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect,useState} from 'react'
+import { Avatar, useChatChannel } from 'stream-chat-react'
+
+import {InviteIcon} from '../assets'
+
+const UserList = () => {
+    return (
+        <div>
+            UserList
+        </div>
+    )
+}
+
+export default UserList
