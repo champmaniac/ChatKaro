5a33ed0764b311d49a7a999b8c0ddd0ee6488333
Prantik Mukherjee <champmaniac04@gmail.com>
2021-09-09 00:41:18 +0530

    make it better

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -25,7 +25,7 @@ const Auth = () => {
         e.preventDefault()
         const {username, phoneNumber, avatarURL, password } = form;
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://champ-chatkaro.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
